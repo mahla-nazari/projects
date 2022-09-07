bab99c61d9de69b58707829968e0a1a8b9af1967
mahla-nazari <112342459+mahla-nazari@users.noreply.github.com>
2022-09-07 21:37:09 +0430

    Update substring.py

--- substring.py
+++ substring.py
@@ -2,3 +2,11 @@ def substring(ch , s):
         while ch in s:
             return True
         return False
+
+# or________use in_func________
+# first, Run in_func
+
+def substring_2(ch , s):
+        while in_func(ch , s)==True:
+                return True
+        return False
