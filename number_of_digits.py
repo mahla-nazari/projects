97dcf5c6f8395de9d41f81134d97e32b7c175cba
mahla-nazari <112342459+mahla-nazari@users.noreply.github.com>
2022-09-07 22:56:55 +0430

    Update number_of_digits.py

--- number_of_digits.py
+++ number_of_digits.py
@@ -1,9 +1,19 @@
 def number_of_digits(n):
-    while type(n)==int:
+    if type(n)!=int:
+        print("Enter a number")
+        
+        
+    while type(n)==int and n>0:
         counter = 0
         while n>0:
             counter+=1
             n//=10
         return counter
-    else:
-        print("Try again")
+    # for Negative numbers
+    while type(n)==int and n<0:
+        counter_2 = 0
+        m = n*-1
+        while m>0:
+            counter_2+=1
+            m//=10
+        return counter_2
