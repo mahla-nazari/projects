be0548890a10eb323ea65dbc35716b1f2b015fc4
mahla-nazari <112342459+mahla-nazari@users.noreply.github.com>
2022-09-07 21:32:19 +0430

    Update reverse_a_number.py

--- reverse_a_number.py
+++ reverse_a_number.py
@@ -1,9 +1,12 @@
 def reverse_a_number(a):
     flag = 0
     while a > 0:
+                
         x = number_of_digits(a)
+        #Run number_of_digits function
         b = (a % 10)
         b*=10**(x-1)
         flag += b
         a//=10
+        
     return flag
