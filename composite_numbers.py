35ebd2c2585c36556adf4ec29c96c8ca8e604ec3
mahla-nazari <112342459+mahla-nazari@users.noreply.github.com>
2022-09-07 21:50:55 +0430

    Update composite_numbers.py

--- composite_numbers.py
+++ composite_numbers.py
@@ -1,9 +1,9 @@
 def composite_numbers(n):
-    if n<=0:
+    if n<=3:
         print("Enter another number")
     else:
-        for i in range(0 , n):
-            for j in range(0, n):
+        for i in range(3 , n):
+            for j in range(3, n):
                 while i<n and j<n and i*j==n:
                     return True
         return False
