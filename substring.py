f719ae267b45d9397290f879c4de0d6836968a9a
mahla-nazari <112342459+mahla-nazari@users.noreply.github.com>
2022-09-07 21:44:02 +0430

    Update substring.py

--- substring.py
+++ substring.py
@@ -1,12 +1,24 @@
 def substring(ch , s):
-        while ch in s:
-            return True
-        return False
+        while len(ch) <= len(s) and type(s)==str:
+                while ch in s:
+                    return True
+                return False
+        else:
+                print("Try again!")
+               
 
 # or________use in_func________
 # first, Run in_func
 
-def substring_2(ch , s):
-        while in_func(ch , s)==True:
-                return True
-        return False
+
+def substring(ch , s):
+        while len(ch) <= len(s) and type(s)==str:
+                while in_func(ch , s)==True:
+                        return True
+                return False
+        else:
+                print("Try again!")
+                        
+                        
+                        
+                           
